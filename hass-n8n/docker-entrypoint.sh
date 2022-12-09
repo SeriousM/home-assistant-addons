5a795fecb5a2508a2bca8ebdd8ba9d41691d4a5d
Bernhard Millauer <bernhard.millauer@gmail.com>
2022-12-09 23:20:11 +0100

    initial import

---
+++ hass-n8n/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#####################
+## USER PARAMETERS ##
+#####################
+
+# REQUIRED
+
+CONFIG_PATH="/data/options.json"
+
+export N8N_BASIC_AUTH_ACTIVE="$(jq --raw-output '.auth // empty' $CONFIG_PATH)"
+export N8N_BASIC_AUTH_USER="$(jq --raw-output '.auth_username // empty' $CONFIG_PATH)"
+export N8N_BASIC_AUTH_PASSWORD="$(jq --raw-output '.auth_password // empty' $CONFIG_PATH)"
+export GENERIC_TIMEZONE="$(jq --raw-output '.timezone // empty' $CONFIG_PATH)"
+export N8N_PROTOCOL="$(jq --raw-output '.protocol // empty' $CONFIG_PATH)"
+export N8N_SSL_CERT="/ssl/$(jq --raw-output '.certfile // empty' $CONFIG_PATH)"
+export N8N_SSL_KEY="/ssl/$(jq --raw-output '.keyfile // empty' $CONFIG_PATH)"
+
+if [ -z "${N8N_BASIC_AUTH_USER}" ] || [ -z "${N8N_BASIC_AUTH_ACTIVE}" ]; then
+    export N8N_BASIC_AUTH_ACTIVE=false
+    unset N8N_BASIC_AUTH_USER
+    unset N8N_BASIC_AUTH_PASSWORD
+fi
+
+###########
+## MAIN  ##
+###########
+
+if [ -d /root/.n8n ] ; then
+  chmod o+rx /root
+  chown -R node /root/.n8n
+  ln -s /root/.n8n /home/node/
+fi
+
+chown -R node /home/node
+
+if [ "$#" -gt 0 ]; then
+  # Got started with arguments
+  exec su-exec node "$@"
+else
+  # Got started without arguments
+  exec su-exec node n8n
+fi
